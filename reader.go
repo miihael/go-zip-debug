c88dc67751fea950fad6783bc9319c8381e6bbeb
Mikhail Anisimov <manisimov@nvidia.com>
2019-10-03 13:01:31 +0300

    correct way to get function name

--- reader.go
+++ reader.go
@@ -44,8 +44,8 @@ type File struct {
 }
 
 func dbg(msg string, args ...interface{}) {
-	_, fn, line, _ := runtime.Caller(1)
-	prefix := fmt.Sprintf("DBG[%d]:%s: ", line, fn)
+	pc, _, line, _ := runtime.Caller(1)
+	prefix := fmt.Sprintf("DBG[%d]:%s: ", line, runtime.FuncForPC(pc).Name())
 	fmt.Fprintf(os.Stderr, prefix+msg+"\n", args...)
 }
 
